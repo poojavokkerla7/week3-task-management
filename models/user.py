00c44ee03eb1267d5397afa90e50156c7bff889a
poojavokkerla7 <pooja.vokkerla@gen2soft.com>
2026-04-10 01:03:51 -0700

    Updated user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,9 @@
-users = []
+from models.db import cursor, conn
+
+def add_user(username, password):
+    cursor.execute("INSERT INTO users VALUES (?, ?)", (username, password))
+    conn.commit()
+
+def get_users():
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()

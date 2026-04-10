4fa1ec274d39bcce3701274354afe013395a82fd
poojavokkerla7 <pooja.vokkerla@gen2soft.com>
2026-04-10 00:53:27 -0700

    Added frontend route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.auth_routes import auth_bp
 from routes.task_routes import task_bp
 
@@ -12,6 +12,9 @@ app.secret_key = "secret123"
 app.register_blueprint(auth_bp)
 app.register_blueprint(task_bp)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
 # Run the app
 if __name__ == "__main__":
     app.run(debug=True)

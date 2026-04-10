f55927bf5fca3d5f988260591f06be6d5d76d1bd
poojavokkerla7 <pooja.vokkerla@gen2soft.com>
2026-04-10 00:39:02 -0700

    Updated auth_routes.py

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,3 +1,8 @@
+from flask import Blueprint, request, jsonify
+from models.user import users
+
+auth_bp = Blueprint("auth", __name__)
+
 @auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
@@ -18,3 +23,17 @@ def register():
     })
 
     return jsonify({"message": "User registered successfully"}), 201
+
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    for user in users:
+        if user["username"] == data.get("username") and user["password"] == data.get("password"):
+            return jsonify({"message": "Login successful"}), 200
+
+    return jsonify({"error": "Invalid credentials"}), 401
